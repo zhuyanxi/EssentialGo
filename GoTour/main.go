1737bea0cca65469299cb38ed481aea765eef520
zhuyanxi <547110356@qq.com>
2020-05-27 17:51:25 +0800

    Update main.go

--- GoTour/main.go
+++ GoTour/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
 
 type Bootcamp struct {
 	Lat float64
@@ -8,10 +11,25 @@ type Bootcamp struct {
 }
 
 func main() {
-	x := new(Bootcamp)
-	y := &Bootcamp{}
-	fmt.Println(&x)
-	fmt.Println(&y)
-	fmt.Println(&*x)
-	fmt.Println(*y)
+	// x := new(Bootcamp)
+	// y := &Bootcamp{}
+	// fmt.Println(&x)
+	// fmt.Println(&y)
+	// fmt.Println(&*x)
+	// fmt.Println(*y)
+
+	name := "eth0"
+	inf, err := net.InterfaceByName(name)
+	if err != nil {
+		fmt.Println("msg", "error getting interface", "inf", name, "err", err)
+	}
+
+	addrs, err := inf.Addrs()
+	if err != nil {
+		fmt.Println("msg", "error getting addresses for interface", "inf", name, "err", err)
+	}
+
+	if len(addrs) <= 0 {
+		fmt.Println("msg", "no addresses found for interface", "inf", name, "err", err)
+	}
 }
